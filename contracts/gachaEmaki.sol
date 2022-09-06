206f15613180447c5a1e3e50ab3c80498d22f61a
lowzzy <60597773+lowzzy@users.noreply.github.com>
2022-09-06 19:51:17 +0900

    ランダムな数値になるように修正など

--- contracts/gachaEmaki.sol
+++ contracts/gachaEmaki.sol
@@ -17,35 +17,60 @@ contract Collection is ERC1155, Ownable {
     using Strings for uint256;
 
     // SS
-    uint256 public constant FUZIN = 8;
-    uint256 public constant RAIZIN = 9;
+    uint256 public constant FUZIN = 9;
+    uint256 public constant RAIZIN = 8;
     // S
-    uint256 public constant YAMATANOROCHI = 6;
-    uint256 public constant KYUBI = 5;
+    uint256 public constant YAMATANOROCHI = 7;
+    uint256 public constant KYUBI = 6;
     // A
-    uint256 public constant KAMAITACHI = 2;
-    uint256 public constant WANYUDO = 3;
-    uint256 public constant GYUKI = 4;
+    uint256 public constant KAMAITACHI = 5;
+    uint256 public constant WANYUDO =4;
+    uint256 public constant GYUKI = 3;
     // B
-    uint256 public constant HANNYA = 7;
-    uint256 public constant TENGU = 0;
-    uint256 public constant YOKO = 1;
+    uint256 public constant HANNYA = 2;
+    uint256 public constant TENGU = 1;
+    uint256 public constant YOKO = 0;
 
+    uint256 randNonce = 0;
+
+    mapping(uint256=>uint256) public prices;
 
     uint256[] rankTokenB = [HANNYA,TENGU,YOKO];
     uint256[] rankTokenA = [KAMAITACHI,WANYUDO,GYUKI];
     uint256[] rankTokenS = [YAMATANOROCHI,KYUBI];
     uint256[] rankTokenSS = [FUZIN,RAIZIN];
-    mapping(uint=>uint[4]) public rankedTokens = [
-        rankTokenB,
-        rankTokenA,
-        rankTokenS,
-        rankTokenSS
+    // mapping(uint=>uint[4]) public rankedTokens;
+
+    uint256[][] public rankedTokens= [
+        rankTokenB,rankTokenA,rankTokenS,rankTokenSS
     ];
+        string name;
+        string symbol;
 
     constructor() ERC1155("") {
-        baseMetadataURIPrefix = "https://staging--goldfish-japan.netlify.app/.netlify/functions/tokenURI/";
+        name ="sample";
+        symbol = "SPL";
+        baseMetadataURIPrefix = "";
         baseMetadataURISuffix = "";
+        uint256 degit = 1000000000000000000;
+
+        // SS
+        prices[FUZIN] = 100 * degit;
+        prices[RAIZIN] = 100 * degit;
+
+        // S
+        prices[YAMATANOROCHI] = 50 * degit;
+        prices[KYUBI] = 50 * degit;
+
+        // A
+        prices[KAMAITACHI] = 30 * degit;
+        prices[WANYUDO] = 30 * degit;
+        prices[GYUKI] = 30 * degit;
+
+        // B
+        prices[YOKO] = 10 * degit;
+        prices[HANNYA] = 10 * degit;
+        prices[TENGU] = 10 * degit;
     }
 
     function uri(uint256 _id) public view override returns (string memory) {
@@ -59,14 +84,23 @@ contract Collection is ERC1155, Ownable {
         _mint(msg.sender, _id, _amount, "");
     }
 
-    function publicMint(uint256 _amount) public {
-        uint256 num = getRandomNum(msg.sender);
-        // uint256 id = 1;
-        uint256 id = getId(num);
-        _mint(msg.sender, id, _amount, "");
+    function publicMint(uint256 _amount) public payable {
+        //　指定の価格で買えるような実装をする
+
     }
 
-    function getId(uint256 _num) public view returns (uint256){
+    function gachaMint(uint256 _amount) public {
+        uint256 num;
+        uint256 id;
+        for (uint256 i = 1; i <= _amount; i++) {
+            num = getRandomNum(msg.sender);
+            id = getId(num);
+            uint256 mintAmount = 1;
+            _mint(msg.sender, id, mintAmount, "");
+        }
+    }
+
+    function getId(uint256 _num) public returns (uint256){
         uint256 id = _num % 100;
         if(0 <= id && id < 50){
             uint256 rankB = 0;
@@ -85,25 +119,22 @@ contract Collection is ERC1155, Ownable {
         return 0;
     }
 
-    function getRandomFromRank(uint256 rank) public view returns (uint256){
+    function getRandomFromRank(uint256 rank) public returns (uint256){
         uint256 len = rankedTokens[rank].length;
-        uint256 tokenId = getRandomNum % len;
+        uint256 tokenId = getRandomNum(msg.sender) % len;
         require(rank < rankedTokens.length,"index is over length.");
         return rankedTokens[rank][tokenId];
     }
 
-    function getRandomNum(address _to) public view returns (uint256){
+    function getRandomNum(address _to) public returns (uint256){
+        randNonce++;
+
         uint256 randomNum = uint256(
             keccak256(
                 abi.encode(
                     _to,
-                    tx.gasprice,
-                    block.number,
                     block.timestamp,
-                    block.difficulty,
-                    blockhash(block.number - 1),
-                    address(this),
-                    1
+                    randNonce
                 )
             )
         );
@@ -114,5 +145,12 @@ contract Collection is ERC1155, Ownable {
         baseMetadataURIPrefix = _prefix;
         baseMetadataURISuffix = _suffix;
     }
+    function changeProbabilities(uint256 _pid,uint256 probability) public onlyOwner() {
+    // 確率いじれるようにする
+    }
+    function changePrices(uint256 _pid,uint256 price) public onlyOwner() {
+        prices[_pid] = price;
+    }
+
 
 }
